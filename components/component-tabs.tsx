21c18da25429f9f6748e8763a6fa97d062fab17c
azacdev <azacdev@gmail.com>
2025-02-16 06:03:57 +0100

    feat: add registry components and styles, implement form input with validation

---
+++ components/component-tabs.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { Index } from "@/__registry__";
+import { useConfig } from "@/hooks/use-config";
+import { styles } from "@/registry/registry-styles";
+import { Tab, Tabs } from "fumadocs-ui/components/tabs";
+
+interface ComponentTabsProps extends React.ComponentPropsWithoutRef<"div"> {
+  name: string;
+  children: React.ReactNode;
+  preventPreviewFocus?: boolean;
+  scalePreview?: boolean;
+  fullPreview?: boolean;
+}
+
+export function ComponentTabs({
+  name,
+  children,
+  preventPreviewFocus,
+  scalePreview,
+  fullPreview,
+  className,
+}: ComponentTabsProps) {
+  const [config] = useConfig();
+  const index = styles.findIndex((style) => style.name === config.style);
+
+  const Codes = React.Children.toArray(children) as React.ReactElement[];
+  const Code = Codes[index];
+
+  const Preview = React.useMemo(() => {
+    const Component = Index[config.style][name]?.component;
+
+    if (!Component) {
+      return (
+        <p className="text-muted-foreground text-sm">
+          Component{" "}
+          <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">
+            {name}
+          </code>{" "}
+          not found in registry.
+        </p>
+      );
+    }
+
+    return <Component />;
+  }, [name, config.style]);
+
+  return (
+    <Tabs items={["Preview", "Code"]} className="rounded-md">
+      <Tab
+        value="Preview"
+        className={cn("preview-block", {
+          "focus-visible:outline-hidden focus-visible:ring-0":
+            preventPreviewFocus,
+        })}
+        tabIndex={preventPreviewFocus ? -1 : 0}
+      >
+        <div
+          className={cn(
+            "flex h-[400px] w-full items-center justify-center p-10",
+            {
+              "h-full p-0": fullPreview,
+              "sm:p-10": scalePreview,
+            },
+            className
+          )}
+        >
+          {Preview}
+        </div>
+      </Tab>
+      <Tab value="Code" className="component-block py-0">
+        {Code}
+      </Tab>
+    </Tabs>
+  );
+}
---
+++ registry/index.ts
@@ -0,0 +1,5 @@
+import type { Registry } from "@/registry/schema";
+
+import { components } from "@/registry/registry-components";
+
+export const registry: Registry = [...components];
